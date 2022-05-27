6a1b22d90b0ea8b95bb5d764b916b7417d2c319b
daniel347 <daniel.siromani@gmail.com>
2022-05-27 00:17:24 +0000

    Nothing significantwq

---
+++ verilog/hardware/processor/source/bubblesort-superh/bsort.c
@@ -0,0 +1,27 @@
+#include "bsort-input.h"
+
+
+int
+main(void)
+{
+	int i;
+	int maxindex = bsort_input_len - 1;
+
+	while (maxindex > 0)
+	{
+		for (i = 0; i < maxindex; i++)
+		{
+			if (bsort_input[i] > bsort_input[i+1])
+			{
+				/*		swap		*/
+				bsort_input[i] ^= bsort_input[i+1];
+				bsort_input[i+1] ^= bsort_input[i];
+				bsort_input[i] ^= bsort_input[i+1];
+			}
+		}
+
+		maxindex--;
+	}
+
+	return 0;
+}
